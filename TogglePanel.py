24e830911e6e87da83b9fce067966ba64401be94
Mike Kormendy <mike@somethinginteractive.com>
2019-02-06 00:20:32 -0600

    Fixed commenting

--- TogglePanel.py
+++ TogglePanel.py
@@ -5,8 +5,8 @@ class TogglePanelCommand(sublime_plugin.WindowCommand):
         ap = self.window.active_panel()
         if ap == panel:
             self.window.run_command("hide_panel", {"panel": panel})
-            // if binding to "escape" key, the following commands will also
-            // hide other panels (ie. find/replace, overlays etc.)
+            # if binding to "escape" key, the following commands will also
+            # hide other panels (ie. find/replace, overlays etc.)
             self.window.run_command("hide_overlay")
             self.window.run_command("hide_panel", {"cancel": "true"})
             self.window.run_command("cancel")
