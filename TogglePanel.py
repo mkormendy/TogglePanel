b47924cded434276f02b0d0389feea5339111fd6
Mike Kormendy <mike@somethinginteractive.com>
2019-02-06 00:19:25 -0600

    Added options to hide other panels too.

--- TogglePanel.py
+++ TogglePanel.py
@@ -1,12 +1,14 @@
-
 import sublime_plugin
-
 
 class TogglePanelCommand(sublime_plugin.WindowCommand):
     def run(self, panel='output.exec'):
         ap = self.window.active_panel()
         if ap == panel:
             self.window.run_command("hide_panel", {"panel": panel})
+            // if binding to "escape" key, the following commands will also
+            // hide other panels (ie. find/replace, overlays etc.)
+            self.window.run_command("hide_overlay")
+            self.window.run_command("hide_panel", {"cancel": "true"})
+            self.window.run_command("cancel")
         else:
             self.window.run_command("show_panel", {"panel": panel})
-
